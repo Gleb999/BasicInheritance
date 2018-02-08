23a1add805f10a0d1841f1f49df4d47827938413
Gleb999 <lihachev99gleb@mail.ru>
2018-02-08 18:17:48 +0700

    Basic inheritance mechanism

---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            Car myCar = new Car(80);
+
+            myCar.Speed = 50;
+            Console.WriteLine("My Car car is going { 0} MPH", myCar.Speed);
+            Console.ReadLine();
+        }
+    }
+}
